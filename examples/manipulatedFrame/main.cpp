b729934c432e54c85191739629a1169f5a30a26b
ATTY Lionel <lionel.atty@ign.fr>
2015-06-09 16:38:40 +0200

    first commit. Clean, normalement compile (sans trop de probleme) sous Mint17. Peut etre encore des soucys avec GLU (pour changer)

--- examples/manipulatedFrame/main.cpp
+++ examples/manipulatedFrame/main.cpp
@@ -1,15 +1,20 @@
 #include "manipulatedFrame.h"
 #include <qapplication.h>
+#include <QtGui>
 
 int main(int argc, char** argv)
 {
+  // Read command lines arguments.
   QApplication application(argc,argv);
 
+  // Create a viewer, show it on screen.
   Viewer viewer;
-
-  viewer.setWindowTitle("manipulatedFrame");
-
   viewer.show();
 
+  // Set the viewer as the application's main widget.
+  //application.setMainWidget(&viewer);
+  application.setActiveWindow(&viewer);
+
+  // Run main loop.
   return application.exec();
 }
